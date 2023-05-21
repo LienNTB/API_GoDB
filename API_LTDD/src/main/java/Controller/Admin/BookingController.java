686064cdf8494a135e35c1a17d75ea40743fbce5
Bích Liên <94447923+LienNTB@users.noreply.github.com>
2023-05-22 02:57:52 +0700

    cập nhật thêm api booking-lấy danh sách các booking đã xác nhận và chưa xác nhận, account-lấy staff qua username

--- API_LTDD/src/main/java/Controller/Admin/BookingController.java
+++ API_LTDD/src/main/java/Controller/Admin/BookingController.java
@@ -21,9 +21,9 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import DAO.AccountDAO;
 import DAO.BookingDAO;
 import DBConnect.DBConnect;
+import Model.Account;
 import Model.Booking;
 import Model.Customer;
-
 
 /**
  * Servlet implementation class BookingController
@@ -31,7 +31,7 @@ import Model.Customer;
 @WebServlet("/admin/booking")
 public class BookingController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	private BookingDAO bookingDAO ;
+	private BookingDAO bookingDAO;
 	private Connection conn;
 
 	public void init() throws ServletException {
@@ -43,18 +43,21 @@ public class BookingController extends HttpServlet {
 			throw new ServletException(e);
 		}
 	}
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public BookingController() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	public BookingController() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		response.setContentType("application/json;charset=UTF-8");
 		response.setCharacterEncoding("UTF-8");
@@ -70,18 +73,20 @@ public class BookingController extends HttpServlet {
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		response.setContentType("application/json;charset=UTF-8");
 		response.setCharacterEncoding("UTF-8");
 		String action = request.getParameter("action");
-	
+
 		if ("search".equals(action)) {
-			String bookingType = request.getParameter("booking_type");
+			String customerId = request.getParameter("customer_id");
 			try {
-				List<Booking> bookings = bookingDAO.getBookingsByType(bookingType);
+				List<Booking> bookings = bookingDAO.getBookingsByCustomerId(customerId);
 				if (bookings != null) {
 					ObjectMapper obj = new ObjectMapper();
 					obj.writeValue(response.getOutputStream(), bookings);
@@ -92,7 +97,7 @@ public class BookingController extends HttpServlet {
 				e.printStackTrace();
 				response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
 			}
-		} 
+		}
 		///////////////////////
 		else if ("add".equals(action)) {
 			String bookingId = request.getParameter("booking_id");
@@ -101,22 +106,23 @@ public class BookingController extends HttpServlet {
 			String staffId = request.getParameter("staff_id");
 			String vehicleId = request.getParameter("vehicle_id");
 			String tourId = request.getParameter("tour_id");
-			String hotelId = request.getParameter("hotel_id");	
+			String hotelId = request.getParameter("hotel_id");
 			String bookingDayString = request.getParameter("booking_date");
 			System.out.println(bookingDayString);
-			
+
 			SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
 			Date bookingDay = null;
 			if (bookingDayString != null && !bookingDayString.isEmpty()) {
-	            try {
-	                java.util.Date utilDate = dateFormat.parse(bookingDayString); // Chuyển đổi chuỗi thành kiểu java.util.Date
-	                bookingDay = new Date(utilDate.getTime()); // Chuyển đổi java.util.Date thành java.sql.Date
-	                System.out.println(utilDate);
-	            } catch (ParseException e) {
-	                e.printStackTrace();
+				try {
+					java.util.Date utilDate = dateFormat.parse(bookingDayString); // Chuyển đổi chuỗi thành kiểu
+																					// java.util.Date
+					bookingDay = new Date(utilDate.getTime()); // Chuyển đổi java.util.Date thành java.sql.Date
+					System.out.println(utilDate);
+				} catch (ParseException e) {
+					e.printStackTrace();
 					System.out.println("Lỗi định dạng date booking rồi bạn ơi");
-	            }
-	        }
+				}
+			}
 			Booking newBooking = new Booking(bookingId, customerId, staffId, vehicleId, tourId, hotelId, bookingDay,
 					bookingType);
 
@@ -128,96 +134,211 @@ public class BookingController extends HttpServlet {
 				response.getWriter().write("Lỗi khi thêm giao dịch.");
 				System.out.println("Lỗi add giao dịch rồi bạn ơi");
 			}
-		}
-			else if ("addTour".equals(action)) {
-//				String bookingId = request.getParameter("booking_id");
-				String customerId = request.getParameter("customer_id");
-				String tourId = request.getParameter("tour_id");	
-				String bookingDayString = request.getParameter("booking_date");
-				System.out.println(bookingDayString);
-				
-				SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
-				Date bookingDay = null;
-				if (bookingDayString != null && !bookingDayString.isEmpty()) {
-		            try {
-		                java.util.Date utilDate = dateFormat.parse(bookingDayString); // Chuyển đổi chuỗi thành kiểu java.util.Date
-		                bookingDay = new Date(utilDate.getTime()); // Chuyển đổi java.util.Date thành java.sql.Date
-		                System.out.println(utilDate);
-		            } catch (ParseException e) {
-		                e.printStackTrace();
-						System.out.println("Lỗi định dạng date booking rồi bạn ơi");
-		            }
-		        }
-				Booking newBooking = new Booking(null, customerId,null, null, tourId, null, bookingDay, null);
+		} else if ("addTour".equals(action)) {
+			String customerId = request.getParameter("customer_id");
+			String tourId = request.getParameter("tour_id");
+			String bookingDayString = request.getParameter("booking_date");
+			System.out.println(bookingDayString);
 
+			SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+			Date bookingDay = null;
+			if (bookingDayString != null && !bookingDayString.isEmpty()) {
 				try {
-					bookingDAO.addBookingTour(newBooking);
-					 JSONObject jsonResponse = new JSONObject();
-			            jsonResponse.put("success", "true");
-			            jsonResponse.put("message", "Đặt tour thành công");
-			            response.setContentType("application/json");
-			            response.setCharacterEncoding("UTF-8");
-			            response.getWriter().write(jsonResponse.toString());
-				} catch (SQLException e) {
+					java.util.Date utilDate = dateFormat.parse(bookingDayString); // Chuyển đổi chuỗi thành kiểu
+																					// java.util.Date
+					bookingDay = new Date(utilDate.getTime()); // Chuyển đổi java.util.Date thành java.sql.Date
+					System.out.println(utilDate);
+				} catch (ParseException e) {
 					e.printStackTrace();
-					response.getWriter().write("Lỗi khi đặt tour.");
-//					System.out.println("Lỗi add giao dịch rồi bạn ơi");
+					System.out.println("Lỗi định dạng date booking rồi bạn ơi");
 				}
-//			
-//			
-//			Customer newCustomer = new Customer(customerId, fullName, email, phoneNumber, imageLink, address, gender,
-//					birthDay);
-//			try {
-//				customerDAO.addCustomer(newCustomer);
-//				response.getWriter().write("Thêm khách hàng thành công.");
-//			} catch (SQLException e) {
-//				e.printStackTrace();
-//				response.getWriter().write("Lỗi khi thêm khách hàng.");
-//				System.out.println("Lỗi add khách hàng rồi bạn ơi");
-//			}
-//		}
-//		////////////////////////////////
-//		else if ("update".equals(action)) {
-//			try {
-//				Customer customer = customerDAO.getCustomerById(customerId);
-//				if (customer == null) {
-//					response.sendError(HttpServletResponse.SC_NOT_FOUND);
-//					return;
-//				}
-//				String fullName = request.getParameter("full_name");
-//				String email = request.getParameter("email");
-//				String phoneNumber = request.getParameter("phone_number");
-//				String imageLink = request.getParameter("image_link");
-//				String address = request.getParameter("address");
-//				boolean gender = Boolean.parseBoolean(request.getParameter("gender"));
-//				String birthDayString = request.getParameter("birth_day");
-//				SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
-//				Date birthDay = null;
-//				if (birthDayString != null && !birthDayString.isEmpty()) {
-//		            try {
-//		                java.util.Date utilDate = dateFormat.parse(birthDayString); // Chuyển đổi chuỗi thành kiểu java.util.Date
-//		                birthDay = new Date(utilDate.getTime()); // Chuyển đổi java.util.Date thành java.sql.Date
-//		                System.out.println(utilDate);
-//		            } catch (ParseException e) {
-//		                e.printStackTrace();
-//						System.out.println("Lỗi định dạng date khách hàng rồi bạn ơi");
-//		            }
-//		        }
-//		            
-//				customer.setFullName(fullName);
-//				customer.setEmail(email);
-//				customer.setPhoneNumber(phoneNumber);
-//				customer.setImageLink(imageLink);
-//				customer.setAddress(address);
-//				customer.setGender(gender);
-//				customer.setBirthDay(birthDay);
-//				customerDAO.updateCustomer(customer);
-//				response.getWriter().write("Cập nhật khách hàng thành công.");
-//			} catch (SQLException e) {
-//				e.printStackTrace();
-//				response.getWriter().write("Lỗi khi cập nhật khách hàng.");
-//			}
-//		}
-	}
+			}
+			Booking newBooking = new Booking(null, customerId, null, null, tourId, null, bookingDay, null);
 
-	}}
+			try {
+				bookingDAO.addBookingTour(newBooking);
+				JSONObject jsonResponse = new JSONObject();
+				jsonResponse.put("success", "true");
+				jsonResponse.put("message", "Đặt tour thành công");
+				response.setContentType("application/json");
+				response.setCharacterEncoding("UTF-8");
+				response.getWriter().write(jsonResponse.toString());
+			} catch (SQLException e) {
+				e.printStackTrace();
+				response.getWriter().write("Lỗi khi đặt tour.");
+			}
+		} else if ("addhotel".equals(action)) {
+			String customerId = request.getParameter("customer_id");
+			String hotelId = request.getParameter("hotel_id");
+			String bookingDayString = request.getParameter("booking_date");
+			System.out.println(bookingDayString);
+
+			SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+			Date bookingDay = null;
+			if (bookingDayString != null && !bookingDayString.isEmpty()) {
+				try {
+					java.util.Date utilDate = dateFormat.parse(bookingDayString); // Chuyển đổi chuỗi thành kiểu
+																					// java.util.Date
+					bookingDay = new Date(utilDate.getTime()); // Chuyển đổi java.util.Date thành java.sql.Date
+
+				} catch (ParseException e) {
+					e.printStackTrace();
+					System.out.println("Lỗi định dạng date booking rồi bạn ơi");
+				}
+			}
+			Booking newBooking = new Booking(null, customerId, null, null, null, hotelId, bookingDay, null);
+
+			try {
+				bookingDAO.addBookingHotel(newBooking);
+				JSONObject jsonResponse = new JSONObject();
+				jsonResponse.put("success", "true");
+				jsonResponse.put("message", "Đặt hotel thành công");
+				response.setContentType("application/json");
+				response.setCharacterEncoding("UTF-8");
+				response.getWriter().write(jsonResponse.toString());
+			} catch (SQLException e) {
+				e.printStackTrace();
+				response.getWriter().write("Lỗi khi đặt hotel.");
+			}
+		} else if ("addVehicle".equals(action)) {
+			String customerId = request.getParameter("customer_id");
+			String VehicleId = request.getParameter("vehicle_id");
+			String bookingDayString = request.getParameter("booking_date");
+			System.out.println(bookingDayString);
+
+			SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+			Date bookingDay = null;
+			if (bookingDayString != null && !bookingDayString.isEmpty()) {
+				try {
+					java.util.Date utilDate = dateFormat.parse(bookingDayString); // Chuyển đổi chuỗi thành kiểu
+																					// java.util.Date
+					bookingDay = new Date(utilDate.getTime()); // Chuyển đổi java.util.Date thành java.sql.Date
+
+				} catch (ParseException e) {
+					e.printStackTrace();
+					System.out.println("Lỗi định dạng date booking rồi bạn ơi");
+				}
+			}
+			Booking newBooking = new Booking(null, customerId, null, VehicleId, null, null, bookingDay, null);
+
+			try {
+				bookingDAO.addBookinVehicle(newBooking);
+				JSONObject jsonResponse = new JSONObject();
+				jsonResponse.put("success", "true");
+				jsonResponse.put("message", "Đặt vehicle thành công");
+				response.setContentType("application/json");
+				response.setCharacterEncoding("UTF-8");
+				response.getWriter().write(jsonResponse.toString());
+			} catch (SQLException e) {
+				e.printStackTrace();
+				response.getWriter().write("Lỗi khi đặt vehicle.");
+			}
+
+		}
+		////////////////////////////////
+		else if ("getforStaff".equals(action)) {
+			response.setContentType("application/json;charset=UTF-8");
+			response.setCharacterEncoding("UTF-8");
+
+			List<Booking> bookings = null;
+			try {
+				bookings = bookingDAO.getBookingForStaff();
+				ObjectMapper obj = new ObjectMapper();
+				obj.writeValue(response.getOutputStream(), bookings);
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		} else if ("getforStaffUsed".equals(action)) {
+			response.setContentType("application/json;charset=UTF-8");
+			response.setCharacterEncoding("UTF-8");
+
+			List<Booking> bookings = null;
+			try {
+				bookings = bookingDAO.getBookingForStaffUsed();
+				ObjectMapper obj = new ObjectMapper();
+				obj.writeValue(response.getOutputStream(), bookings);
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		} else if ("searchId".equals(action)) {
+			try {
+				String bookingId = request.getParameter("booking_id");
+				Booking booking = bookingDAO.getBookingById(bookingId);
+				if (booking != null) {
+					ObjectMapper obj = new ObjectMapper();
+					obj.writeValue(response.getOutputStream(), booking);
+				} else {
+					response.sendError(HttpServletResponse.SC_NOT_FOUND);
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+				response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+			}
+		} else if ("updateBookingforStaff".equals(action)) {
+			String bookingId = request.getParameter("booking_id");
+			try {
+				Booking booking = bookingDAO.getBookingById(bookingId);
+				if (booking == null) {
+					response.sendError(HttpServletResponse.SC_NOT_FOUND);
+					return;
+				}
+				String staffId = request.getParameter("staff_id");
+				booking.setStaffId(staffId);
+				bookingDAO.updateStatusBooking(booking);
+				JSONObject jsonResponse = new JSONObject();
+				jsonResponse.put("success", "true");
+				jsonResponse.put("message", "Cập nhật trạng thái đơn hàng thành công");
+				response.setContentType("application/json");
+				response.setCharacterEncoding("UTF-8");
+				response.getWriter().write(jsonResponse.toString());
+			} catch (SQLException e) {
+				e.printStackTrace();
+				response.getWriter().write("Lỗi khi cập nhật tình trạng xác nhận.");
+			}
+
+		} else if ("deleteBooking".equals(action)) {
+			String bookingId = request.getParameter("booking_id");
+			try {
+				Booking booking = bookingDAO.getBookingById(bookingId);
+				if (booking == null) {
+					response.sendError(HttpServletResponse.SC_NOT_FOUND);
+					return;
+				}
+				bookingDAO.deleteBooking(booking);
+				JSONObject jsonResponse = new JSONObject();
+				jsonResponse.put("success", "true");
+				jsonResponse.put("message", "Xóa đơn hàng thành công");
+				response.setContentType("application/json");
+				response.setCharacterEncoding("UTF-8");
+				response.getWriter().write(jsonResponse.toString());
+			} catch (SQLException e) {
+				e.printStackTrace();
+				response.getWriter().write("Lỗi khi xóa đơn hàng");
+			}
+		} else if ("arrange".equals(action)) {
+			response.setContentType("application/json;charset=UTF-8");
+			response.setCharacterEncoding("UTF-8");
+
+			List<Booking> bookings = null;
+			try {
+				bookings = bookingDAO.ArrangeBookings();
+				ObjectMapper obj = new ObjectMapper();
+				obj.writeValue(response.getOutputStream(), bookings);
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		} else if ("arrangedesc".equals(action)) {
+			response.setContentType("application/json;charset=UTF-8");
+			response.setCharacterEncoding("UTF-8");
+
+			List<Booking> bookings = null;
+			try {
+				bookings = bookingDAO.ArrangeBookingsDESC();
+				ObjectMapper obj = new ObjectMapper();
+				obj.writeValue(response.getOutputStream(), bookings);
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
